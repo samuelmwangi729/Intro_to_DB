cf29de56074e6d8682f9f2acf47953fc62e05e55
Samuel Mwangi <42680795+samuelmwangi729@users.noreply.github.com>
2025-08-17 19:32:35 +0300

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Script that prints the full description of the table `books`
+Script that prints the full description of the table `Books`
 from the database `alx_book_store`.
 The database name will be passed as an argument to the script.
 """
@@ -22,17 +22,17 @@ if __name__ == "__main__":
         db = MySQLdb.connect(host="localhost", user=username, passwd=password, db=db_name)
         cursor = db.cursor()
 
-        # Query INFORMATION_SCHEMA to get full description of `books`
+        # Query INFORMATION_SCHEMA to get full description of `Books`
         query = """
         SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
         FROM INFORMATION_SCHEMA.COLUMNS
-        WHERE TABLE_SCHEMA = %s AND TABLE_NAME = 'books';
+        WHERE TABLE_SCHEMA = %s AND TABLE_NAME = 'Books';
         """
         cursor.execute(query, (db_name,))
         rows = cursor.fetchall()
 
         # Print results in a readable format
-        print("Full description of `books` table:")
+        print("Full description of `Books` table:")
         for row in rows:
             print(row)
 
