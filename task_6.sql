e8b171b411379d1f061aad14983efb279379080d
Samuel Mwangi <42680795+samuelmwangi729@users.noreply.github.com>
2025-08-17 19:36:40 +0300

    Update task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -25,13 +25,13 @@ if __name__ == "__main__":
 
     cur = db.cursor()
 
-    # Insert multiple rows
+    # Insert multiple rows with explicit "124 Happiness Ave."
     sql = """
-    INSERT INTO customer (customer_id, customer_name, email, address)
+    INSERT IGNORE INTO customer (customer_id, customer_name, email, address)
     VALUES
-    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-    (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+        (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+        (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
+        (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
     """
 
     try:
