190693a3b90281a6a0ce9bd0f846b7a8565ea720
samuelmwangi729 <samuelmwangi729@gmail.com>
2025-08-17 19:14:05 +0300

    created the sql file

---
+++ alx_book_store.sql
@@ -0,0 +1,36 @@
+CREATE TABLE IF NOT EXISTS Authors (
+    author_id INTEGER PRIMARY KEY,
+    author_name VARCHAR(215) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Customers (
+    customer_id INTEGER PRIMARY KEY,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215),
+    address TEXT
+);
+
+CREATE TABLE IF NOT EXISTS Books (
+    book_id INTEGER PRIMARY KEY,
+    title VARCHAR(130) NOT NULL,
+    author_id INTEGER NOT NULL,
+    price DOUBLE NOT NULL,
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
+
+CREATE TABLE IF NOT EXISTS Orders (
+    order_id INTEGER PRIMARY KEY,
+    customer_id INTEGER NOT NULL,
+    order_date DATE NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+
+CREATE TABLE IF NOT EXISTS Order_Details (
+    orderdetailid INTEGER PRIMARY KEY,
+    order_id INTEGER NOT NULL,
+    book_id INTEGER NOT NULL,
+    quantity DOUBLE NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
